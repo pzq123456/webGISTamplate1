e42519b7781f3a78b7a08cf72a74edc2cc4ad751
PanZhiQing <panzhiqing15@gmail.com>
2025-06-27 15:00:50 +0800

    feat: 更新主题配置，启用导航菜单以改善用户体验

--- docs/.vitepress/themeConfig.ts
+++ docs/.vitepress/themeConfig.ts
@@ -1,9 +1,8 @@
 export const themeConfig = {
     // https://vitepress.dev/reference/default-theme-config
-    // nav: [
-    //   { text: 'Home', link: '/' },
-    //   { text: 'Examples', link: '/markdown-examples' }
-    // ],
+    nav: [
+      { text: 'Home', link: '/' },
+    ],
     // footer: {
     //     copyright: 'GlobalEVData © 2024 - Present | <a href="https://thetipteam.editorx.io/website/">The TIP Research Group</a> @ <a href="https://www.polyu.edu.hk/"> HKPolyU </a>'
     // },
